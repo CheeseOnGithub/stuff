44b4acf0b6b9d89d6085a079e28511bea4d5bc0b
CheeseOnGithub <cheeseisaretard1@gmail.com>
2025-04-26 08:51:37 +0200

    fent

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local req = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
+local httpsService = cloneref(game:GetService("HttpService"))
+
+if not req then
+    messagebox("executor not supported dm me on discord")
+end
+
+local res = req({
+    Url = "https://raw.githubusercontent.com/CheeseOnGithub/stuff/refs/heads/main/scripts/" .. game.GameId .. ".lua"
+})
+
+if res.Success then
+    loadstring(res.Body)
+end
